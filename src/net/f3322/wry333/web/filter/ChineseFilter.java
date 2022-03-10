5b4ea6fc71aae9bf9c38c21a43927acb1d2abfe7
WRY <wry233333@gmail.com>
2022-03-10 20:05:39 +0800

    filter

---
+++ src/net/f3322/wry333/web/filter/ChineseFilter.java
@@ -0,0 +1,32 @@
+package net.f3322.wry333.web.filter;
+
+
+import com.sun.deploy.net.HttpRequest;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter("/*")
+public class ChineseFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        request.setCharacterEncoding("utf-8");
+        filterChain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
