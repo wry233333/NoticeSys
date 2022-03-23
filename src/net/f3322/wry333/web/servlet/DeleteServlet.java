477c95a748388f356586a91eeacbfb0ad4ae5856
28214 <2523332048@qq.com>
2022-03-23 18:07:27 +0800

    upload

---
+++ src/net/f3322/wry333/web/servlet/DeleteServlet.java
@@ -0,0 +1,29 @@
+package net.f3322.wry333.web.servlet;
+
+import net.f3322.wry333.bean.Book;
+import net.f3322.wry333.service.DeleteService;
+import net.f3322.wry333.service.DeleteServiceImpl;
+import net.f3322.wry333.utils.ParaUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] ids = req.getParameterValues("b_id");
+        DeleteService service = new DeleteServiceImpl();
+        service.delete(ids);
+        req.getRequestDispatcher("/FindServlet").forward(req, resp);
+    }
+}
